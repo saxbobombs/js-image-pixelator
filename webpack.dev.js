2df32e9961e822209ffceb1ba940612c52c6e71d
Hendrik Sachse <hendrik.sachse@gmail.com>
2022-09-25 20:49:11 +0200

    update dev dependencies, webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,13 +2,14 @@ var path = require('path');
 
 module.exports = {
 	entry: './src/js/main.js',
+	mode: 'development',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 	},
 
 	devServer: {
 		open: false,
-		contentBase: [
+		static: [
 			path.join(__dirname, 'src'),
 		],
 		port: 8000,
